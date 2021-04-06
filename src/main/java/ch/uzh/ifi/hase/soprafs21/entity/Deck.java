34603824be9140868f3704dc06dc2a6ee14b516b
Yves-Meister <yves.meister@uzh.ch>
2021-04-06 14:02:53 +0200

    Update Deck.java

--- src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Deck.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Deck.java
@@ -9,14 +9,15 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
-import ch.uzh.ifi.hase.soprafs21.constant.GameRole;
+import ch.uzh.ifi.hase.soprafs21.entity.PlayCard;
+
 
 @Entity
 public class Deck {
-
+    PlayCard PlayCard = new PlayCard();
 
     public PlayCard drawCard(){
-
+        
    }
 
     public PlayCard discardCard(){
