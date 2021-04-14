06cc6d5b523ecc39bf625bfe0f83299068c28661
Elexa Heggli <elexa.heggli@gmx.ch>
2021-04-14 14:59:08 +0200

    added OnFieldCardsRepository and OnFieldCardsService
    still waiting on Hand branch to finish implementation

--- src/main/java/ch/uzh/ifi/hase/soprafs21/entity/OnFieldCards.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entity/OnFieldCards.java
@@ -11,16 +11,35 @@ public class OnFieldCards {
     @OneToMany(cascade = CascadeType.ALL)
     private List<PlayCard> onFieldCards;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getLength(){
+        List<PlayCard> temp = onFieldCards;
+        return temp.size();
+    }
+
     public List<PlayCard> getOnFieldCards() {
         return onFieldCards;
     }
 
-    public void addCard(BlueCard card, Player player){
+    public void setOnFieldCards(List<PlayCard> cards) {
+        for (int i = 0; i < cards.size(); i++) {
+            onFieldCards.add(cards.get(0));
+        }
+    }
+
+    public void addOnFieldCard(BlueCard card){
         onFieldCards.add(card);
     }
 
-    public void changeOwner(BlueCard card, Player prevOwner, Player newOwner) {
-
+    public void removeOnFieldCard(BlueCard card) {
+        onFieldCards.remove(card);
     }
 
 }
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/repository/OnFieldCardsRepository.java
@@ -0,0 +1,7 @@
+package ch.uzh.ifi.hase.soprafs21.repository;
+
+import ch.uzh.ifi.hase.soprafs21.entity.OnFieldCards;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OnFieldCardsRepository extends JpaRepository<OnFieldCards, Long>{
+}
