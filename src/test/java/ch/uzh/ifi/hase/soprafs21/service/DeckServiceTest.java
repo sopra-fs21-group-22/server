a419e518bd008ed1c3212a83b7d3e7c3fbf90033
yvmeis <71340078+yvmeis@users.noreply.github.com>
2021-04-15 11:43:03 +0200

    see last commit

--- src/test/java/ch/uzh/ifi/hase/soprafs21/service/DeckServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/DeckServiceTest.java
@@ -4,6 +4,7 @@ import ch.uzh.ifi.hase.soprafs21.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs21.entity.Deck;
 import ch.uzh.ifi.hase.soprafs21.entity.User;
 import ch.uzh.ifi.hase.soprafs21.repository.DeckRepository;
+import ch.uzh.ifi.hase.soprafs21.entity.PlayCard;
 import ch.uzh.ifi.hase.soprafs21.repository.UserRepository;
 
 //import org.hibernate.mapping.List;
