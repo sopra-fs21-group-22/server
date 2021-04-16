887adf790fe61f703bd07ad7a0a0e3073ea0640e
Robert Bommeli <robert.bommeli@gmail.com>
2021-04-16 22:09:53 +0200

    added new exception GameLogicException

--- src/main/java/ch/uzh/ifi/hase/soprafs21/exceptions/GlobalExceptionAdvice.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/exceptions/GlobalExceptionAdvice.java
@@ -25,13 +25,13 @@ public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
 
     private final Logger log = LoggerFactory.getLogger(GlobalExceptionAdvice.class);
 
-    @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
+    @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class, GameLogicException.class })
     protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
         String bodyOfResponse = ex.getMessage();
         return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
-    @ExceptionHandler(TransactionSystemException.class)
+    @ExceptionHandler(value = { TransactionSystemException.class })
     public ResponseStatusException handleTransactionSystemException(Exception ex, HttpServletRequest request) {
         log.error("Request: {} raised {}", request.getRequestURL(), ex);
         return new ResponseStatusException(HttpStatus.CONFLICT, ex.getMessage(), ex);
