cd8b8ab1849274dd499f9a40d96af2c3a2b0ca56
Yves-Meister <yves.meister@uzh.ch>
2021-04-29 15:05:09 +0200

    Update PlayerTableService.java

--- src/main/java/ch/uzh/ifi/hase/soprafs21/service/PlayerTableService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/service/PlayerTableService.java
@@ -53,9 +53,6 @@ public class PlayerTableService {
 
     @Autowired
     DeckService deckService;
-    
-    @Autowired
-    CharacterService characterService;
 
     @Autowired
     OnFieldCardsService onFieldCardsService;
@@ -86,7 +83,7 @@ public class PlayerTableService {
                 players.add(player);
                 playerTable.setPlayers(players);
                 player.setTable(playerTable);
-                player.setCharacterCard(characterService.pickCharacter(player, playerTable));
+                //player.setCharacterCard(this.pickCharacter(player, playerTable));
                 playerTableRepository.save(playerTable);
                 playerTableRepository.flush();
 
@@ -103,8 +100,8 @@ public class PlayerTableService {
         playerTable.setPlayers(players);
         playerTable.setDeck(deck);
         playerTable.setDiscardPile(discardPile);
-        playerTable.setCharacterPile(characterService.makeCharacterPile());
-        player.setCharacterCard(characterService.pickCharacter(player, playerTable));
+        playerTable.setCharacterPile(this.makeCharacterPile());
+        //player.setCharacterCard(this.pickCharacter(player, playerTable));
         playerTableRepository.save(playerTable);
         playerTableRepository.flush();
         return playerTable;
@@ -119,6 +116,60 @@ public class PlayerTableService {
     // }
     // playerTableRepository.save(table);
     // }
+
+    public CharacterPile makeCharacterPile() {
+        CharacterPile characterPile = new CharacterPile();
+        List<CharacterCard> characterCards = new ArrayList<CharacterCard>();
+        CharacterCard characterCard1 = new CharacterCard("Willy The Kid", 4);
+        CharacterCard characterCard2 = new CharacterCard("Rose Doolan", 4);
+        CharacterCard characterCard3 = new CharacterCard("Paul Regret", 3);
+        CharacterCard characterCard4 = new CharacterCard("Jourdonnais", 4);
+        CharacterCard characterCard5 = new CharacterCard("Bart Cassidy", 4);
+        CharacterCard characterCard6 = new CharacterCard("Suzy Lafayette", 4);
+        CharacterCard characterCard7 = new CharacterCard("El Gringo", 3);
+
+        characterCards.add(characterCard1);
+        characterCards.add(characterCard2);
+        characterCards.add(characterCard3);
+        characterCards.add(characterCard4);
+        characterCards.add(characterCard5);
+        characterCards.add(characterCard6);
+        characterCards.add(characterCard7);
+
+        characterPile.setCharacterCards(characterCards);
+        return characterPile;
+
+    }
+
+    public CharacterCard pickCharacter(Player player, PlayerTable table) {
+        Random rand = new Random();
+        CharacterCard card = table.getCharacterPile().getCharacterCards()
+                .get(rand.nextInt(table.getCharacterPile().getCharacterCards().size()));
+        table.getCharacterPile().getCharacterCards()
+                .remove(rand.nextInt(table.getCharacterPile().getCharacterCards().size()));
+        player.setCharacterCard(card);
+        player.setMaxBullets(player.getCharacterCard().getLifeAmount());
+        player.setBullets(player.getCharacterCard().getLifeAmount());
+        //playerTableRepository.save(table);
+
+        /* if (player.getCharacterCard().getName().equals("Willy The Kid")) {
+            player.setPlayableBangsAnyRound(255);
+            player.setStillPlayableBangsThisRound(255);
+        }
+
+        if (player.getCharacterCard().getName().equals("Rose Doolan")) {
+            player.setBaseRange(2);
+            player.setRange(2);
+        }
+
+        if (player.getCharacterCard().getName().equals("Paul Regret")) {
+            player.setDistanceIncreaseForOthers(1);
+        } */
+
+        playerTableRepository.save(table);
+        playerTableRepository.flush();
+        return card;
+    }
 
     public PlayerTable getPlayerTableById(Long id) {
         return playerTableRepository.getOne(id);
