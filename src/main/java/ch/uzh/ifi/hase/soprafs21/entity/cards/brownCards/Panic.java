2410b5021f25c2aaed95d1ed4f29dc8cfeeb0074
Robert Bommeli <robert.bommeli@gmail.com>
2021-05-05 14:11:36 +0200

    minor bug fix

--- src/main/java/ch/uzh/ifi/hase/soprafs21/entity/cards/brownCards/Panic.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entity/cards/brownCards/Panic.java
@@ -3,6 +3,8 @@ package ch.uzh.ifi.hase.soprafs21.entity.cards.brownCards;
 import java.util.List;
 import java.util.Random;
 
+import javax.persistence.Entity;
+
 import ch.uzh.ifi.hase.soprafs21.constant.Card;
 import ch.uzh.ifi.hase.soprafs21.constant.Rank;
 import ch.uzh.ifi.hase.soprafs21.constant.Suit;
@@ -10,6 +12,7 @@ import ch.uzh.ifi.hase.soprafs21.entity.Player;
 import ch.uzh.ifi.hase.soprafs21.entity.cards.PlayCard;
 import ch.uzh.ifi.hase.soprafs21.rest.dto.game.PayLoadDTO;
 
+@Entity
 public class Panic extends BrownCard {
     public Panic() {
     }
