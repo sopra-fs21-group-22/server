a324ff941f5f49577c39eeb97f1c2907506c61b2
Yves-Meister <yves.meister@uzh.ch>
2021-05-06 21:00:58 +0200

    comment out player death on strikeout

--- src/main/java/ch/uzh/ifi/hase/soprafs21/service/PlayerTableService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/service/PlayerTableService.java
@@ -279,10 +279,10 @@ public class PlayerTableService {
         else if(table.getTimeRemaining()<0L) { 
             table.getPlayerOnTurn().setStrikes(table.getPlayerOnTurn().getStrikes() + 1);
 
-            table.getPlayerOnTurn().setBullets(0);
+            /* table.getPlayerOnTurn().setBullets(0);
 
             List<PlayCard> handCards = table.getPlayerOnTurn().getHand().getPlayCards();
-            List<BlueCard> onFieldCards = table.getPlayerOnTurn().getOnFieldCards().getOnFieldCards();
+            List<BlueCard> onFieldCards = table.getPlayerOnTurn().getOnFieldCards().getOnFieldCards();          // this kills the player after 3 strikes
             for (PlayCard card : handCards) {
                 table.getPlayerOnTurn().getTable().getDiscardPile().addCard(card);
             }
@@ -292,7 +292,7 @@ public class PlayerTableService {
             table.getPlayerOnTurn().getHand().setPlayCards(new ArrayList<>());
             table.getPlayerOnTurn().getOnFieldCards().removeAllCards();
 
-            nextPlayersTurn(table);
+            nextPlayersTurn(table); */
         }
     }
 
